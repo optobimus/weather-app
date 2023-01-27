2350b8b6cf62ceb8ee8486f27dfcf6d1274a04cb
Tobias <113835194+optobimus@users.noreply.github.com>
2023-01-27 21:30:07 +0100

    Complete setup, add basic html layout & bg image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import css from 'styles.css';
+import css from './styles.css';
+
+
+function fetchData(location) {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=c623d6c1dc938ebc64fffc73f94df621`, {mode: 'cors'}) 
+}
+
+function processData(weatherData) {
+    weatherData.then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        console.log(response);
+    });
+}
+
+const weatherData = fetchData("Brixen");
+processData(weatherData);
